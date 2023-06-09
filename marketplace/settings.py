91a918bc39812e777d84bdcae175dec7bd15c43a
kietn20 <kietnguyen3698@gmail.com>
2023-06-09 15:51:35 -0700

    postgresql

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.parse(env("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    "default": dj_database_url.parse(env("DATABASE_URL"))
+}
 
 # DATABASES['default'] = dj_database_url.config(
 #     conn_max_age=None,
